d3d54798dad00c8aa687c4bc8300d3e13029d830
LarahBrandao <larah1108@hotmail.com>
2020-07-29 01:17:59 -0300

    Finaliza aula 02 e add modificações pessoais

--- src/pages/Cadastro/Categoria/index.js
+++ src/pages/Cadastro/Categoria/index.js
@@ -6,7 +6,18 @@ function CadastroCategoria() {
     return (
       <PageDefault>
         
-        <h1>Cadastro de Categoria</h1>
+      <h1>Cadastro de Categoria</h1>
+
+        <form>
+          <label>
+            Nome da Categoria:
+            <input type="text"/>
+          </label>
+
+          <button>
+            Cadastrar
+          </button>      
+        </form>
 
         <Link to="/">Ir para home</Link>
 
