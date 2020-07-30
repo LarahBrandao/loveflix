ce553bfe1c9c52db66cdfeb087348b151ed7fa72
LarahBrandao <larah1108@hotmail.com>
2020-07-29 23:41:27 -0300

    Add mof footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,7 +8,7 @@ function Footer() {
         <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
       </a>
       <p>
-        Orgulhosamente criado durante a
+        Orgulhosamente criado por <a href="https://www.linkedin.com/in/larahbrandao/">Larah Brandão</a> durante a
         {' '}
         <a href="https://www.alura.com.br/">
           Imersão React da Alura
