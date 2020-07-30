2f2e1f20039bd23de3cd63620547e9b2d2e0a866
LarahBrandao <larah1108@hotmail.com>
2020-07-30 19:37:42 -0300

    configs

--- src/pages/Cadastro/Categoria/index.js
+++ src/pages/Cadastro/Categoria/index.js
@@ -32,7 +32,7 @@ function CadastroCategoria() {
       }
 
   useEffect( () => {
-    const URL = "http://localhost:8080/categorias"
+    const URL = "https://loveflix-server.herokuapp.com/categorias"
     fetch(URL).then(async(respostaDoServer) => {
       if(respostaDoServer.ok) {
         const resposta = await respostaDoServer.json();
