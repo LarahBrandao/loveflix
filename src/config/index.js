4b24ecb207347e28d8ddc44b99fc9ae26a56661e
Larah Brandão <55256624+LarahBrandao@users.noreply.github.com>
2020-08-19 12:27:44 -0300

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,8 @@
 const URL_BACKEND =  window.location.hostname.includes('localhost')
-? "http://localhost:8080"
-: "https://loveflix-server.herokuapp.com"
+    ? 'http://localhost:8080'
+    : 'https://loveflix-server.herokuapp.com';
 
 
 export default {
-    URL_BACKEND   
+    URL_BACKEND,   
 };
