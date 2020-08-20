a6e344215d415b9815930c3bed64d6158605074e
Larah Brandão <55256624+LarahBrandao@users.noreply.github.com>
2020-08-20 15:24:49 -0300

    Update index.js

--- src/components/Carousel/components/VideoCard/index.js
+++ src/components/Carousel/components/VideoCard/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { VideoCardContainer } from './styles';
+import { VideoCardContainer, VideoCardBanner } from './styles';
 
 function getYouTubeId(youtubeURL) {
   return youtubeURL
@@ -12,6 +12,7 @@ function getYouTubeId(youtubeURL) {
 
 function VideoCard({ videoTitle, videoURL, categoryColor }) {
   const image = `https://img.youtube.com/vi/${getYouTubeId(videoURL)}/hqdefault.jpg`;
+  
   return (
     <VideoCardContainer
       url={image}
@@ -19,7 +20,15 @@ function VideoCard({ videoTitle, videoURL, categoryColor }) {
       target="_blank"
       style={{ borderColor: categoryColor || 'red' }}
       title={videoTitle}
-    />
+    >
+    
+      <VideoCardBanner style={{ backgroundColor: categoryColor || 'red' }}> 
+
+
+        {videoTitle} 
+
+      </VideoCardBanner>
+    </VideoCardContainer>
   );
 }
 
